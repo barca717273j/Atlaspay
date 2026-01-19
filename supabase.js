6b4d4530b30453e662a2c8b3830a1aca5c75bfc1
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-19 19:38:39 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
+<script>
+  const supabaseUrl = "SUA_PROJECT_URL_AQUI";
+  const supabaseKey = "SUA_ANON_KEY_AQUI";
+
+  const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+</script>
