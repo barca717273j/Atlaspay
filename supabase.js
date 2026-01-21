6acad34b008f83c835952bf4cd66351cbe8e3cbe
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-21 10:17:27 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 <script src="https://unpkg.com/@supabase/supabase-js@2"></script>
 <script>
-  const supabaseUrl = "SUA_PROJECT_URL_AQUI";
-  const supabaseKey = "SUA_ANON_KEY_AQUI";
+  const supabaseUrl = "https://vkfuvijagxwkmuposbya.supabase.co";
+  const supabaseKey = "grant select on table api.<your_table> to anon; grant select, insert, update, delete on table api.<your_table> to authenticated;";
 
   const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 </script>
