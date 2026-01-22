326df427af9eee1ef31fc0c28a3f8fcf7c6d6db1
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-22 01:41:06 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://vkfuvijagxwkmuposbya.supabase.co'
-const SUPABASE_ANON_KEY = 'vkfuvijagxwkmuposbya'
+const SUPABASE_ANON_KEY = 'sb_publishable_qCYTPLr0Aw4red76wgcYFg_lWOEP_3o'
 
 export const supabase = createClient(
   SUPABASE_URL,
