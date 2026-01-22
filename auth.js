ef67440070f733ca45b8ab1a0627f757e3fa270b
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-22 01:39:23 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const SUPABASE_URL = 'https://vkfuvijagxwkmuposbya.supabase.co'
-const SUPABASE_ANON_KEY = 'grant select on table api.<your_table> to anon; grant select, insert, update, delete on table api.<your_table> to authenticated;'
+const SUPABASE_ANON_KEY = 'vkfuvijagxwkmuposbya'
 
 export const supabase = createClient(
   SUPABASE_URL,
