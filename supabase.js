7599771f95441c4b99f49afe65bbff3d09bb5a75
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-25 02:12:11 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-<script src="https://unpkg.com/@supabase/supabase-js@2"></script>
-<script>
-  const supabaseUrl = "https://vkfuvijagxwkmuposbya.supabase.co";
-  const supabaseKey = "grant select on table api.<your_table> to anon; grant select, insert, update, delete on table api.<your_table> to authenticated;";
-
-  const supabase = supabase.createClient(supabaseUrl, supabaseKey);
-</script>
