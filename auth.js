f1248ee036a0528a84aecc32c05b092bd227728a
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-25 13:20:02 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 const supabaseUrl = 'https://vkfuvijagxwkmuposbya.supabase.co'
-const supabaseAnonKey = 'sb_publishable_qCYTPLr0Aw4red76wgcYFg_lWOEP_3o'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZnV2aWphZ3h3a211cG9zYnlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NjExMTUsImV4cCI6MjA4NDQzNzExNX0.O1-1PACGNTz9aUAwItWFgGBiaoEgvigDel8ezaSJAJs'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
