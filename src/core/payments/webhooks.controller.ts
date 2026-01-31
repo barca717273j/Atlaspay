ae110d2bf703a7895c3a16bf001fdb0f786c8513
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-31 02:04:38 -0300

    Delete src/core/payments directory

--- src/core/payments/webhooks.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-import { processWebhook } from './payments.service'
-
-export async function handleWebhook(req: Request, res: Response) {
-  await processWebhook(req.body)
-  res.sendStatus(200)
-}
