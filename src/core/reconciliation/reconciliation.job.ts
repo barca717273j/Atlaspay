b126be2eb557d9fde112632f8ea4a14d07b4490c
barca717273j <140835124+barca717273j@users.noreply.github.com>
2026-01-31 02:02:47 -0300

    Delete src/core directory

--- src/core/reconciliation/reconciliation.job.ts
+++
@@ -1,6 +0,0 @@
-export async function runReconciliation() {
-  // 1. Buscar transações do Mercado Pago (API)
-  // 2. Buscar transações do AtlasPay
-  // 3. Comparar
-  // 4. Se faltar alguma, alertar
-}
